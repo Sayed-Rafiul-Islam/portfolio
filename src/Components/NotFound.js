1a93e2cd63c092b7c82495491f276e2cb248410d
Yasin-Rafid <96468660+Yasin-Rafid@users.noreply.github.com>
2022-06-16 03:15:25 +0600

    done

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -8,7 +8,7 @@ const NotFound = () => {
         navigate('/home')
     }
     return (
-        <div className='body404 mx-auto text-center relative'>
+        <div className='body404 mx-auto text-center relative overflow-hidden'>
             <h1 className='absolute four text-teal-500'><b>404</b></h1>
             <p className='text-gray-400 lg:mt-80 mt-96 pt-12 lg:pt-0'>Looks like you are lost</p>
             <button onClick={handleHome} className='btn-home mt-6'>Go Back</button>
